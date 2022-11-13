3fe80e153e0c50c5484333f03af1ec484541215b
Rekiichan <dtnhan.ete.dut@gmail.com>
2022-11-13 21:53:42 +0700

    reup main

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import pandas as pd
 from statistical import test
+from get_feature import get_feature, get_features
+from write_data import write_file
 
 if __name__ == "__main__":
+    
+    # get feature and write data feature
+    
+    suplo_feature, tomato_feature = get_features()
+    write_file(suplo_feature,'data/suplo_data/suplo_feature')
+    write_file(tomato_feature,'data/tomato_data/tomato_feature')
+    
+    # load data and thong ke
+    
     file_path = 'data/train'
     train_data_suplo = pd.read_csv(
         f'{file_path}/train_data_suplo.csv').to_numpy()
@@ -23,7 +34,4 @@ if __name__ == "__main__":
     print(f'RC: {RC}')
     print(f'PR: {PR}')
     print(f'ACC: {ACC}')
-    # for i in range (10):
-    #     res = predict(train_data_tomato, train_data_suplo, test_data_suplo[i], 3)
-    #     print(res)
 
